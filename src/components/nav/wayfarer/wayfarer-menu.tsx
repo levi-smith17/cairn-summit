eb7791d4fa61ebd3a987be567f06ae9ec9a3e0c8
levi-smith17 <levi_smith17@icloud.com>
2026-03-09 17:40:10 -0400

    fix wayfarer-menu if no avatar exists

--- src/components/nav/wayfarer/wayfarer-menu.tsx
+++ src/components/nav/wayfarer/wayfarer-menu.tsx
@@ -50,7 +50,7 @@ export function NavUser({ wayfarer }: NavUserProps) {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={wayfarer.avatar} alt={wayfarer.name} />
+                <AvatarImage src={wayfarer.avatar ?? undefined} alt={wayfarer.name ?? undefined} />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
@@ -69,7 +69,7 @@ export function NavUser({ wayfarer }: NavUserProps) {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={wayfarer.avatar} alt={wayfarer.name} />
+                  <AvatarImage src={wayfarer.avatar ?? undefined} alt={wayfarer.name ?? undefined} />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
