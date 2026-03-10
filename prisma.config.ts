f5bf50cee904015ee2140c21b7df9b057f1df13a
levi-smith17 <levi_smith17@icloud.com>
2026-03-10 14:47:37 -0400

    fix prisma schema problem causing failed generate

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,5 +5,5 @@ import * as dotenv from 'dotenv'
 dotenv.config({ path: path.resolve(__dirname, '.env') })
 
 export default defineConfig({
-  schema: path.join(__dirname, 'prisma/models'),
+  schema: path.join(__dirname, 'prisma/'),
 })
