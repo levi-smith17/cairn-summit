c7cbb5dd6307f4a0a4fa742d987d5cd3492365f9
Levi Smith <levi_smith17@icloud.com>
2026-05-17 19:16:17 -0400

    chore: minor terraform updates

--- infrastructure/terraform/environments/prod/s3.tf
+++ infrastructure/terraform/environments/prod/s3.tf
@@ -4,9 +4,10 @@ module "s3" {
   managed_by      = var.managed_by
   owner           = var.owner
   project_name    = var.project_name
+  custom_domain   = "media.${var.domain}"
+  certificate_arn = aws_acm_certificate_validation.cloudfront.certificate_arn
   allowed_origins = [
-    "http://localhost:3000",
-    "http://localhost:5173",
+    "https://${var.domain}",
     module.cloudfront.cloudfront_url,
   ]
 }
